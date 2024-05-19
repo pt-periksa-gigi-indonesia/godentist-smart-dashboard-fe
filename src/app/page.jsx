a1cecc6d4606149f102be852fd9848a6f4ad7210
rifqifazzam <rifqifauzan709@gmail.com>
2024-05-20 01:08:50 +0700

    refactor: update page.jsx to use next/navigation instead of next/router

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
+"use client";
+
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function RootRedirect() {
     const router = useRouter();
 
     React.useEffect(() => {
         router.push('/dashboard');
-    }, []);
+    }, [router]);
 
     return null; 
 }
