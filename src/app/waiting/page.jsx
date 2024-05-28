321faf2cb3a2741c28015199cc8dd8e0f0524f95
mochammadkevin <kevinariobimo@gmail.com>
2024-05-29 00:57:54 +0700

    refactor: waiting page component

--- src/app/waiting/page.jsx
+++ src/app/waiting/page.jsx
@@ -9,7 +9,6 @@ export default function WaitingPage() {
   const [isVerified, setIsVerified] = useState(false);
   const [userRole, setUserRole] = useState('');
 
-
   const fetchUserRole = async () => {
     try {
       const role = await getUserRole();
@@ -22,36 +21,34 @@ export default function WaitingPage() {
     }
   };
 
-
   useEffect(() => {
     fetchUserRole();
   }, []);
-  
 
   const handleLoginRedirect = () => {
     router.push("/login");
   };
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100 p-8">
-      <div className="flex flex-col items-center bg-white p-12 rounded-3xl shadow-lg">
+    <main className="flex min-h-screen items-center justify-center bg-gray-100 p-4 md:p-8">
+      <div className="flex flex-col items-center bg-white p-6 md:p-12 rounded-3xl shadow-lg w-full max-w-md md:max-w-lg lg:max-w-xl">
         <Image
           src="/static/images/godentist_logo.jpeg"
           alt="Logo"
           width={1488}
           height={423}
-          className="max-w-xs md:max-w-sm lg:max-w-base mb-8"
+          className="max-w-xs md:max-w-sm lg:max-w-base mb-4 md:mb-8 px-12 md:px-0"
         />
-        <h1 className="text-3xl font-bold text-blue-dentist mb-4">Registration Successful!</h1>
+        <h1 className="text-2xl md:text-3xl font-bold text-blue-dentist mb-4 text-center md:text-left">Registration Successful!</h1>
         {isVerified ? (
-        <p className="text-gray-700 text-lg mb-4">
-          Your account has been verified. You can now log in.
-        </p>
-      ) : (
-        <p className="text-gray-700 text-lg mb-4">
-          Your account is pending verification by the master admin. You will be notified once your account has been verified.
-        </p>
-      )}
+          <p className="text-gray-700 text-base md:text-lg mb-4 text-center md:text-left">
+            Your account has been verified. You can now log in.
+          </p>
+        ) : (
+          <p className="text-gray-700 text-base md:text-lg mb-4 text-center md:text-left">
+            Your account is pending verification by the master admin. You will be notified once your account has been verified.
+          </p>
+        )}
         <button
           onClick={handleLoginRedirect}
           className="w-full p-3 bg-blue-dentist text-white rounded-lg font-medium hover:bg-blue-dentist-dark"
