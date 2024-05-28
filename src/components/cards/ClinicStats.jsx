1a16d3268f867285d04b6c4531ae48459a259aa3
mochammadkevin <kevinariobimo@gmail.com>
2024-05-28 23:33:13 +0700

    update: ClinicStats grid layout

--- src/components/cards/ClinicStats.jsx
+++ src/components/cards/ClinicStats.jsx
@@ -5,7 +5,7 @@ import StatCard from '@/components/ui/statCard';
 const ClinicStats = ({ totalClinics, totalTransactions }) => {
   return (
     <div className="col-span-2 p-4 bg-gradient-to-r from-blue-500 to-blue-600 rounded-2xl border border-gray-200 shadow-md">
-      <div className="grid grid-cols-2 gap-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
         <StatCard Icon={FaHospital} label="Total Clinics" value={totalClinics !== undefined ?`${totalClinics} clinics`:"-"} />
         <StatCard Icon={FaMoneyCheckAlt} label="Total Transactions" value={totalClinics !== undefined ? `Rp ${totalTransactions} `:"Rp -"} />
       </div>
