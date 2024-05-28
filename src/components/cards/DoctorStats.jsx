58aaa687851e3c8a21c5e071679ee0839c9b76e3
mochammadkevin <kevinariobimo@gmail.com>
2024-05-28 23:33:22 +0700

    update: DoctorStats component grid layout

--- src/components/cards/DoctorStats.jsx
+++ src/components/cards/DoctorStats.jsx
@@ -5,7 +5,7 @@ import StatCard from '@/components/ui/statCard';
 const DoctorStats = ({ totalDoctor, totalUnverDoctor, totalVerDoctor }) => {
   return (
     <div className="col-span-2 p-4 bg-gradient-to-r from-blue-500 to-blue-600 rounded-2xl border border-gray-200 shadow-md">
-      <div className="grid grid-cols-3 gap-2">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
         <StatCard Icon={FaUser} label="Total Doctor" value={totalDoctor !== undefined ?`${totalDoctor} doctors`: "-"} />
         <StatCard Icon={FaUserClock} label="Unverified" value={totalUnverDoctor !== undefined ?`${totalUnverDoctor} doctors`:"-"} />
         <StatCard Icon={FaUserCheck} label="Verified" value={totalVerDoctor !== undefined ?`${totalVerDoctor} doctors`:"-"} />
