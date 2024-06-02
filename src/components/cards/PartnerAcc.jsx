af15a20d8c11be11a4dd0d6d7903203fa69d5b66
mochammadkevin <kevinariobimo@gmail.com>
2024-06-02 22:36:44 +0700

    feat: add new StatCard components

--- src/components/cards/PartnerAcc.jsx
+++ src/components/cards/PartnerAcc.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { FaUserClock, FaUserCheck, FaClinicMedical } from "react-icons/fa";
 import StatCard from '@/components/ui/statCard';  
+import StatCard2 from '@/components/ui/statCard2';  
+import StatCard3 from '@/components/ui/statCard3';  
+
 
 const PartnerStats = ({ totalUnverDoctor, totalVerDoctor, totalClinics }) => {
   return (
@@ -8,8 +11,8 @@ const PartnerStats = ({ totalUnverDoctor, totalVerDoctor, totalClinics }) => {
       <h2 className="text-md font-medium text-gray-100 mb-2">Partner's Account</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
         <StatCard Icon={FaUserClock} label="Unverified" value={(totalUnverDoctor !== undefined && (totalUnverDoctor >= 0)) ?`${totalUnverDoctor} doctors`:"-"} />
-        <StatCard Icon={FaUserCheck} label="Verified" value={(totalVerDoctor !== undefined ) && (totalVerDoctor >= 0)?`${totalVerDoctor} doctors`:"-"} />
-        <StatCard Icon={FaClinicMedical} label="Total Clinics" value={(totalClinics !== undefined) && (totalClinics >= 0)  ?`${totalClinics} clinics`:"-"} />
+        <StatCard2 Icon={FaUserCheck} label="Verified" value={(totalVerDoctor !== undefined ) && (totalVerDoctor >= 0)?`${totalVerDoctor} doctors`:"-"} />
+        <StatCard3 Icon={FaClinicMedical} label="Total Clinics" value={(totalClinics !== undefined) && (totalClinics >= 0)  ?`${totalClinics} clinics`:"-"} />
       </div>
     </div>
   );
