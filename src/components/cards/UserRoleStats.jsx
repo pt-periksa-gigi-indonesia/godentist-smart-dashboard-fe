e4e8f40d6306e9efa19b2f9b2a1400d16f95a841
mochammadkevin <kevinariobimo@gmail.com>
2024-06-03 00:55:19 +0700

    update: StatCard component import in UserRoleStats.jsx

--- src/components/cards/UserRoleStats.jsx
+++ src/components/cards/UserRoleStats.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FaUser, FaUserShield } from 'react-icons/fa';
-import StatCard from '@/components/ui/statCard';
+import StatCard4 from '@/components/ui/statCard4';
 
 const UserRoleStats = ({ totalUsers, totalAdmins }) => {
     return (
         <div className="col-span-2 p-4 bg-gradient-to-r from-blue-500 to-blue-600 rounded-2xl border border-gray-200 shadow-md">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <StatCard Icon={FaUser} label="Total Users" value={totalUsers} />
-                <StatCard Icon={FaUserShield} label="Total Admins" value={totalAdmins} />
+                <StatCard4 Icon={FaUser} label="Total Users" value={totalUsers} />
+                <StatCard4 Icon={FaUserShield} label="Total Admins" value={totalAdmins} />
             </div>
         </div>
     );
