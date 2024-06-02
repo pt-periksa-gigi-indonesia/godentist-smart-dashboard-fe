680f9a97a9e944ffb1bcce3a8297fc56e540f44c
rifqifazzam <rifqifauzan709@gmail.com>
2024-06-02 13:56:09 +0700

    refactor: update seedHandler.js and seedButton.jsx

--- src/api/lib/seedHandler.js
+++ src/api/lib/seedHandler.js
@@ -4,7 +4,7 @@ export async function seedData() {
     const cookies = await getCookies();
     const access_token = cookies.access_token.value;
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/seed`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/seed/latest`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -16,3 +16,21 @@ export async function seedData() {
 
     return data;
 }
+
+export async function seedLatest() {
+    const cookies = await getCookies();
+    const access_token = cookies.access_token.value;
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/seed/latest`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${access_token}`
+        },
+    });
+
+    const data = await response.json();
+
+    return data;
+}
+
--- src/components/seedButton.jsx
+++ src/components/seedButton.jsx
@@ -1,41 +1,57 @@
-import { seedData } from '@/api/lib/seedHandler';
+import { seedData, seedLatest } from '@/api/lib/seedHandler';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { Skeleton } from './ui/skeleton';
 
 export default function SeedButton() {
-    const [seedMessage, setSeedMessage] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+    const [isFetchingLastSeed, setIsFetchingLastSeed] = useState(false);
+    const [lastSeed, setLastSeed] = useState('');
+
+    const fetchLastSeedDate = async () => {
+        setIsFetchingLastSeed(true);
+        try {
+            const response = await seedLatest();
+            const formattedDate = new Date(response.createdAt).toLocaleString();
+            setLastSeed(formattedDate);
+        } catch (error) {
+            console.error('Error fetching last seed date:', error);
+        } finally {
+            setIsFetchingLastSeed(false);
+        }
+    };
 
     const handleSeedDatabase = async () => {
-        setIsLoading(true);
-        setSeedMessage(null);
+        setIsFetchingLastSeed(true);
         try {
-            const response = await seedData();
-            setSeedMessage(response.message);
-            console.log(response);
+            await seedData();
+            console.log("Database seeding successful.");
         } catch (error) {
             console.error('Error seeding database:', error);
-            setSeedMessage('Error occurred while seeding the database.');
             console.log("failed");
+        }finally {
+            await fetchLastSeedDate();
         }
 
         setTimeout(() => {
-            setIsLoading(false);
-            setSeedMessage("Database seeded successfully!");
             window.location.reload();
         }, 2000);
     };
 
+    useEffect(() => {
+        fetchLastSeedDate();
+    }, []);
+
     return (
-        <div div className="flex ml-5 items-center space-x-4 justify-end">
-            {isLoading ? (
-                <p className="text-sm">Loading...</p>
+        <div className="flex ml-5 items-center space-x-4 justify-end">
+            {isFetchingLastSeed ? (
+                <Skeleton className="h-4 w-24" />
             ) : (
-                seedMessage && <p className="text-sm">{seedMessage}</p>
+                <p className='md:text-sm sm:text-xs'>Seeded at: {lastSeed}</p>
             )}
             <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full flex items-center"
