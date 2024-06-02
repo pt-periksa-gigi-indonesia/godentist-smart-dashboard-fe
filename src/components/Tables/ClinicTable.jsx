c2e0e064fbab67ec2ed085b7ecb1565c6c5bc9e6
mochammadkevin <kevinariobimo@gmail.com>
2024-06-03 00:55:26 +0700

    update: ClinicTable styles

--- src/components/Tables/ClinicTable.jsx
+++ src/components/Tables/ClinicTable.jsx
@@ -8,19 +8,19 @@ import {
 
 export default function ClinicTable({ clinics }) {
     return (
-        <div className="mt-6 mb-6 bg-white rounded-lg border border-gray-200 shadow-sm">
+        <div className="mt-6 mb-6 bg-white rounded-2xl border border-gray-200 shadow-sm">
             {clinics && clinics.length > 0 ? (
                 <Table>
-                    <thead className="bg-gray-50">
+                    <thead className="bg-gradient-to-r from-blue-500 to-blue-600">
                         <tr>
-                            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider rounded-tl-2xl">
                                 Clinic Name
                             </th>
-                            <th scope="col" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col" className="px-6 py-3 text-center text-xs font-medium text-white uppercase tracking-wider">
                                 Total Transactions
                             </th>
-                            <th scope="col" className="relative px-6 py-3">
-                                <span className="sr-only">Details</span>
+                            <th scope="col" className="relative px-6 py-3 rounded-tr-2xl">
+                                <span className="sr-only ">Details</span>
                             </th>
                         </tr>
                     </thead>
