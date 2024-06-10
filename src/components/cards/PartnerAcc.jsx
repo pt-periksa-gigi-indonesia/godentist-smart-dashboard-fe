4b8d3eb87987f6104813a0d4b9eff7a8b87853a8
mochammadkevin <kevinariobimo@gmail.com>
2024-06-10 13:12:52 +0700

    feat: update Partner's Account heading style

--- src/components/cards/PartnerAcc.jsx
+++ src/components/cards/PartnerAcc.jsx
@@ -8,7 +8,7 @@ import StatCard3 from '@/components/ui/statCard3';
 const PartnerStats = ({ totalUnverDoctor, totalVerDoctor, totalClinics }) => {
   return (
     <div className="col-span-2 p-4 bg-gradient-to-r from-blue-500 to-blue-600 rounded-xl border border-gray-200 shadow-sm">
-      <h2 className="text-md font-medium text-gray-100 mb-2">Partner's Account</h2>
+      <h2 className="text-md font-medium text-gray-100 mt-2 mb-4">Partner's Account</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
         <StatCard Icon={FaUserClock} label="Unverified" value={(totalUnverDoctor !== undefined && (totalUnverDoctor >= 0)) ?`${totalUnverDoctor} doctors`:"-"} />
         <StatCard2 Icon={FaUserCheck} label="Verified" value={(totalVerDoctor !== undefined ) && (totalVerDoctor >= 0)?`${totalVerDoctor} doctors`:"-"} />
