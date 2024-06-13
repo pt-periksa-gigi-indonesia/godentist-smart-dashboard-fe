7707fc917720d688a254810347f4c7f6d37a279c
rifqifazzam <rifqifauzan709@gmail.com>
2024-06-13 19:59:13 +0700

    refactor: update statCard components with new background colors

--- src/components/ui/statCard5.jsx
+++ src/components/ui/statCard5.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StatCard5 = ({ Icon, label, value }) => {
   return (
-    <div className="p-2 bg-gradient-to-r from-yellow-500 to-yellow-600 text-gray-100 rounded-xl flex items-center">
+    <div className="p-2 bg-red-stat text-gray-100 rounded-xl flex items-center">
       <Icon className="text-xl ml-4 mr-4" />
       <div>
         <h3 className="text-md font-normal">{label}</h3>
--- src/components/ui/statCard6.jsx
+++ src/components/ui/statCard6.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StatCard6 = ({ Icon, label, value }) => {
   return (
-    <div className="p-2 bg-gradient-to-r from-green-500 to-green-600 text-gray-100 rounded-xl flex items-center">
+    <div className="p-2 bg-green-stat text-gray-100 rounded-xl flex items-center">
       <Icon className="text-xl ml-4 mr-4" />
       <div>
         <h3 className="text-md font-normal">{label}</h3>
